502325fd9469c70364bf69bc4319a3eabe0d7944
Kyle Guthery <ksguthery@gmail.com>
2022-03-13 17:33:40 -0500

    first commit

---
+++ caesar-cipher.rb
@@ -0,0 +1,38 @@
+# Assignment=> Implement a caesar cipher that takes in a string and the shift factor and then outputs the modified string.
+
+def caesar_cipher(string, shift)
+  new_string = ""
+  alphabet = "abcdefghijklmnopqrstuvwxyz"
+  string.each_char { 
+    |character| 
+    if alphabet.include?(character.downcase)
+      char_location = alphabet.index(character.downcase).to_i
+      new_location = char_location + shift
+      if new_location > 25
+        new_location = new_location - 26
+      end
+      new_character = alphabet[new_location]
+      if alphabet.include?(character)
+        new_string << new_character
+      else
+        new_string << new_character.upcase
+      end
+    else
+      new_string << character
+    end
+  }
+  puts new_string
+  return new_string
+end
+
+# caesar_cipher("What a string!", 5)
+
+puts "Input string:"
+
+string = gets.chomp
+
+puts "Input shift amount (whole numbers only):"
+
+shift = gets.chomp.to_i
+
+caesar_cipher(string, shift)
