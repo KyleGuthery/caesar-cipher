36d08fb3590906ed25d8a4dde536976263d0a05e
Kyle Guthery <ksguthery@gmail.com>
2022-03-14 21:46:17 -0500

    remove assignment comment

--- caesar-cipher.rb
+++ caesar-cipher.rb
@@ -1,5 +1,3 @@
-# Assignment: Implement a caesar cipher that takes in a string and the shift factor and then outputs the modified string.
-
 def caesar_cipher(string, shift)
   new_string = ""
   alphabet = "abcdefghijklmnopqrstuvwxyz"
